1f681b8a7732a8e17c4edddd3756b627a07809ba
Rockn26 <irfanank26@gmail.com>
2024-02-24 16:19:45 +0300

    add filter functionality

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,9 +2,20 @@ import React, { useEffect, useState } from "react";
 import "./Main.style.css";
 import Item from "../Item/Item";
 
-
 const Main = () => {
   const [items, setItems] = useState([]);
+  const [filteredItems, setFilteredItems] = useState([]);
+  const [filterState, setFilterState] = useState("all");
+
+  useEffect(() => {
+    if (filterState === "all") {
+      setFilteredItems(items);
+    } else if (filterState === "active") {
+      setFilteredItems(items.filter((item) => !item.isCompleted));
+    } else if (filterState === "completed") {
+      setFilteredItems(items.filter((item) => item.isCompleted));
+    }
+  }, [items, filterState]);
 
   const handleUserInput = (e) => {
     const object = {
@@ -12,14 +23,14 @@ const Main = () => {
       isCompleted: false,
       isRemoved: false,
     };
-    setItems(prevItems => [...prevItems, object]);
+    setItems((prevItems) => [...prevItems, object]);
     e.target.value = "";
   };
 
   useEffect(() => {
     const keyDownHandler = (e) => {
       if (e.key === "Enter") {
-        e.preventDefault(); 
+        e.preventDefault();
         handleUserInput(e);
       }
     };
@@ -31,26 +42,29 @@ const Main = () => {
     };
   }, []);
 
-  
-
   return (
     <div className="main-container">
       <input type="text" placeholder="What needs to be done" />
       <div className="items">
-          {items.map((item) => {
-            return (
-              <Item key={item.task} item={item} items={items} setItems={setItems} />
-            );
-          })}
+        {filteredItems.map((item) => {
+          return (
+            <Item
+              key={item.task}
+              item={item}
+              items={items}
+              setItems={setItems}
+            />
+          );
+        })}
       </div>
       <div className="items-info">
-        <span> items left </span>
+        <span> {items.filter(obj=>!obj.isCompleted).length} items left </span>
         <div className="filter-buttons">
-          <button>all</button>
-          <button>active</button>
-          <button>completed</button>
+          <button onClick={() => setFilterState("all")}>all</button>
+          <button onClick={() => setFilterState("active")}>active</button>
+          <button onClick={()=> setFilterState("completed")}>completed</button>
         </div>
-        <button>clear completed</button>
+        <button onClick={() => setItems(obj=> items.filter(obj=> !obj.isCompleted))}>clear completed</button>
       </div>
     </div>
   );
